c1a64f8cc3e3339dc9170b97593d5bc4f3211d36
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-04 00:48:52 -0300

    feat: implementation of the users insert

--- src/main/java/br/com/sabrinaweb/appbiblioteca/model/dao/impl/UserDaoJdbc.java
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/model/dao/impl/UserDaoJdbc.java
@@ -2,10 +2,14 @@ package br.com.sabrinaweb.appbiblioteca.model.dao.impl;
 
 import br.com.sabrinaweb.appbiblioteca.model.dao.UserDao;
 import br.com.sabrinaweb.appbiblioteca.model.entities.User;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
+@Log4j2
 public class UserDaoJdbc implements UserDao {
     private Connection conn;
 
@@ -15,12 +19,24 @@ public class UserDaoJdbc implements UserDao {
 
     @Override
     public void insert(User user) {
-        
+        String sql = "INSERT INTO library.user (name, email, phone, address) VALUES (?, ?, ?, ?)";
+        log.info("Saving the user '{}'", user.getName());
+
+        try (PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPhone());
+            ps.setString(4, user.getAddress());
+
+            ps.execute();
+        }catch (SQLException e){
+            log.error("Error trying to insert the user");
+        }
     }
 
     @Override
     public void update(User user) {
-
+        
     }
 
     @Override
