2eb60acaa904e050e1e3660948ca8703c07ee6a1
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-04 13:01:37 -0300

    update: creation of find by name and find by id methods

--- src/main/java/br/com/sabrinaweb/appbiblioteca/model/dao/UserDao.java
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/model/dao/UserDao.java
@@ -3,11 +3,13 @@ package br.com.sabrinaweb.appbiblioteca.model.dao;
 import br.com.sabrinaweb.appbiblioteca.model.entities.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserDao {
     void insert(User user);
     void update(User user);
     void deleteById(Integer id);
     List<User> findAllUsers();
-    List<User> findByName();
+    List<User> findByName(String name);
+    Optional<User> findById(Integer id);
 }
