a9160665e9b1da9a57364980c163fc92c48c8731
Sabrina Gomes <sabrina.gomess.dev@gmail.com>
2025-07-11 17:50:22 -0300

    creating the main menu

---
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/view/MenuMain.java
@@ -0,0 +1,13 @@
+package br.com.sabrinaweb.appbiblioteca.view;
+
+import br.com.sabrinaweb.appbiblioteca.service.AuthorService;
+import br.com.sabrinaweb.appbiblioteca.service.BookService;
+import br.com.sabrinaweb.appbiblioteca.service.LibraryLoanService;
+import br.com.sabrinaweb.appbiblioteca.service.UserService;
+
+public class MenuMain {
+    private BookService bookService;
+    private AuthorService authorService;
+    private LibraryLoanService libraryLoanService;
+    private UserService userService;
+}
