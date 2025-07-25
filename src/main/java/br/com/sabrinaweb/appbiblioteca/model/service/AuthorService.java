44a58c13ed3528e4a948c1989e08ce3358e25ef1
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-25 15:39:16 -0300

    refactor: refactoring the author service class to be able to implement unit tests and creating an author service test to test its methods

--- src/main/java/br/com/sabrinaweb/appbiblioteca/model/service/AuthorService.java
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/model/service/AuthorService.java
@@ -9,93 +9,92 @@ import lombok.extern.log4j.Log4j2;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 @Log4j2
 public class AuthorService {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private Scanner scanner;
+    private AuthorDao authorDao;
+
     private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    private final AuthorDao authorDao = DaoFactory.createAuthorDao();
+
+    public AuthorService(Scanner scanner, AuthorDao authorDao) {
+        this.scanner = scanner;
+        this.authorDao = authorDao;
+    }
+
+    public AuthorService() {
+    }
 
     public void insert() {
-        try {
-            System.out.println("Enter the author name to insert");
-            String name = SCANNER.nextLine();
-            System.out.println("Enter the nationality of the author: ");
-            String nationality = SCANNER.nextLine();
-            System.out.println("Enter the author birthdate: (yyyy-MM-dd)");
-            String birthDate = SCANNER.nextLine();
-            if (name.isEmpty() | nationality.isEmpty() | birthDate.isEmpty()) throw new IllegalArgumentException("You need to enter all fields to insert a valid author, otherwise you won't be able to insert the author");
-            Author author = Author.builder().name(name).nationality(nationality).birthdate(LocalDate.parse(birthDate, fmt)).build();
-            authorDao.insert(author);
-        }catch (IllegalArgumentException e){
-            log.error(e.getMessage());
-        }
+        System.out.println("Enter the author name to insert");
+        String name = scanner.nextLine();
+        System.out.println("Enter the nationality of the author: ");
+        String nationality = scanner.nextLine();
+        System.out.println("Enter the author birthdate: (yyyy-MM-dd)");
+        String birthDate = scanner.nextLine();
+
+        if (name.isEmpty() | nationality.isEmpty() | birthDate.isEmpty()) throw new IllegalArgumentException("You need to enter all fields to insert a valid author, otherwise you won't be able to insert the author");
+        Author author = Author.builder().name(name).nationality(nationality).birthdate(LocalDate.parse(birthDate, fmt)).build();
+        authorDao.insert(author);
     }
 
     public void delete() {
-        try{
-            authorDao.findAllAuthors().forEach(a -> System.out.printf("ID:[%d] - %s, %s, %s %n", a.getId(), a.getName(), a.getNationality(), a.getBirthdate()));
+        authorDao.findAllAuthors().forEach(a -> System.out.printf("ID:[%d] - %s, %s, %s %n", a.getId(), a.getName(), a.getNationality(), a.getBirthdate()));
 
-            System.out.println("Enter the author id to delete");
-            Integer id = Integer.parseInt(SCANNER.nextLine());
+        System.out.println("Enter the author id to delete");
+        Integer id = Integer.parseInt(scanner.nextLine());
 
-            if (id < 0) throw new InvalidIdException("The id is null or equal 0, you should enter a valid id");
+        invalidId(id);
 
-            System.out.printf("Are you sure that you want to delete the author: '%d' [Y/N] %n", id);
-            System.out.print("RESP: ");
-            String resp = SCANNER.nextLine();
-            if (resp.equalsIgnoreCase("y")) authorDao.deleteById(id);
-        }catch (NumberFormatException e){
-            log.error(e.getMessage());
-        }
+        System.out.printf("Are you sure that you want to delete the author: '%d' [Y/N] %n", id);
+        System.out.print("RESP: ");
+        String resp = scanner.nextLine();
+        if (resp.equalsIgnoreCase("y")) authorDao.deleteById(id);
     }
 
     public void update() {
-        try {
-            System.out.println("Enter the author id to find");
-            Integer id = Integer.parseInt(SCANNER.nextLine());
-            Author authorFoundById = authorDao.findById(id).orElseThrow(() -> new InvalidIdException("The id is invalid"));
-            log.info("ID:[{}] - {} - {} - {}", authorFoundById.getId(), authorFoundById.getName(), authorFoundById.getNationality(), authorFoundById.getBirthdate());
-            System.out.println("Enter the new author name or empty to keep the same");
-            String name = SCANNER.nextLine();
-            System.out.println("Enter the new author nationality or empty to keep the same: ");
-            String nationality = SCANNER.nextLine();
-            System.out.println("Enter the author birthdate: (yyyy-mm-dd) or empty to keep the same");
-            String birthDate = SCANNER.nextLine();
+        System.out.println("Enter the author id to find");
+        Integer id = Integer.parseInt(scanner.nextLine());
+        invalidId(id);
+        Author authorFoundById = authorDao.findById(id).orElseThrow(() -> new IllegalArgumentException("No author was found with the specified id"));
+        log.info("ID:[{}] - {} - {} - {}", authorFoundById.getId(), authorFoundById.getName(), authorFoundById.getNationality(), authorFoundById.getBirthdate());
+        System.out.println("Enter the new author name or empty to keep the same");
+        String name = scanner.nextLine();
+        System.out.println("Enter the new author nationality or empty to keep the same: ");
+        String nationality = scanner.nextLine();
+        System.out.println("Enter the author birthdate: (yyyy-mm-dd) or empty to keep the same");
+        String birthDate = scanner.nextLine();
 
-            name = name.isEmpty() ? authorFoundById.getName() : name;
-            nationality = name.isEmpty() ? authorFoundById.getNationality() : nationality;
-            birthDate = birthDate.isEmpty() ? authorFoundById.getBirthdate().toString() : birthDate;
+        name = name.isEmpty() ? authorFoundById.getName() : name;
+        nationality = name.isEmpty() ? authorFoundById.getNationality() : nationality;
+        birthDate = birthDate.isEmpty() ? authorFoundById.getBirthdate().toString() : birthDate;
 
-            Author userToUpdate = Author.builder()
-                    .id(authorFoundById.getId())
-                    .name(name)
-                    .nationality(nationality)
-                    .birthdate(LocalDate.parse(birthDate, fmt))
-                    .build();
-            authorDao.update(userToUpdate);
-        }catch (NumberFormatException e){
-            log.error(e.getMessage());
-        }
+        Author authorToUpdate = Author.builder()
+                .id(authorFoundById.getId())
+                .name(name)
+                .nationality(nationality)
+                .birthdate(LocalDate.parse(birthDate, fmt))
+                .build();
+        authorDao.update(authorToUpdate);
     }
 
     public void findByName() {
         System.out.println("Enter the author name to found");
-        String name = SCANNER.nextLine();
-        List<Author> userByName = authorDao.findByName(name);
-        log.info("Author(s) found '{}'", userByName);
+        String name = scanner.nextLine();
+        List<Author> authorByName = authorDao.findByName(name);
+        log.info("Author(s) found '{}'", authorByName);
     }
 
     public void findAuthorByWroteBook() {
-        try {
-            System.out.print("Enter the book id to find its author(s): ");
-            int id = Integer.parseInt(SCANNER.nextLine());
-            if (id < 0) throw new InvalidIdException("The id is null or equal 0, you should enter a valid id");
-            List<Author> authorsOfABook = authorDao.findAuthorByWroteBook(id);
-            log.info("Author(s) found '{}'", authorsOfABook);
-        }catch (NumberFormatException e){
-            System.out.println(e.getMessage());
-        }
+        System.out.print("Enter the book id to find its author(s): ");
+        int id = Integer.parseInt(scanner.nextLine());
+        invalidId(id);
+        List<Author> authorsOfABook = authorDao.findAuthorByWroteBook(id);
+        log.info("Author(s) found '{}'", authorsOfABook);
+    }
+    public void invalidId(Integer id) {
+        if (id <= 0) throw new InvalidIdException("The id is lower or equal 0, you should enter a valid id");
     }
 }
---
+++ src/test/java/br/com/sabrinaweb/appbiblioteca/model/service/AuthorServiceTest.java
@@ -0,0 +1,144 @@
+package br.com.sabrinaweb.appbiblioteca.model.service;
+
+import br.com.sabrinaweb.appbiblioteca.model.dao.AuthorDao;
+import br.com.sabrinaweb.appbiblioteca.model.entities.Author;
+import br.com.sabrinaweb.appbiblioteca.model.exceptions.InvalidIdException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorServiceTest {
+    private AuthorDao authorDao;
+    private AuthorService authorService;
+
+    @BeforeEach
+    public void setUp(){
+        authorDao = new AuthorDao() {
+            @Override
+            public void insert(Author author) {
+                System.out.println("Simulated insertion to test the author");
+            }
+            @Override
+            public void deleteById(Integer id) {
+                System.out.println("Simulated exclusion to test the author");
+            }
+            @Override
+            public void update(Author author) {
+                System.out.println("Simulated updated to test the author");
+            }
+            @Override
+            public List<Author> findAllAuthors() {
+                System.out.println("Simulated findAllAuthors to test the author");
+                return List.of();
+            }
+            @Override
+            public List<Author> findByName(String name) {
+                System.out.println("Simulated findByName to test the author");
+                return List.of();
+            }
+            @Override
+            public Optional<Author> findById(Integer id) {
+                System.out.println("Simulated findById to test the author");
+                return Optional.empty();
+            }
+            @Override
+            public List<Author> findAuthorByWroteBook(Integer idBook) {
+                System.out.println("Simulated findByWroteBook to test the author");
+                return List.of();
+            }
+        };
+    }
+    @Test
+    @DisplayName("If the fields are empty, an IllegalArgumentException will be thrown")
+    void insert_ThrowIllegalArgumentException_WhenFieldsAreEmpty() {
+        String scannerTest = "\n\n\n\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> authorService.insert());
+    }
+
+    @Test
+    @DisplayName("If the fields are not empty, an IllegalArgumentException won't be thrown")
+    void insert_DoesNotThrowIllegalArgumentException_WhenFieldsAreAllFillIn() {
+        String scannerTest = "ExampleName\nNationality\n2012-03-25";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertDoesNotThrow(() -> authorService.insert());
+    }
+
+    @Test
+    @DisplayName("If the id is null a NumberFormatException should be thrown")
+    void deleteById_ThrowNumberFormatException_WhenFieldIsEmpty(){
+        String scannerTest = "\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertThrows(NumberFormatException.class, () -> authorService.delete());
+    }
+
+    @Test
+    @DisplayName("If the id is lower or equal than zero, an InvalidIdException should be thrown")
+    void invalidId_ThrowInvalidIdException_WhenIdIsLowerOrEqualThanZero(){
+        authorService = new AuthorService();
+        Assertions.assertThrows(InvalidIdException.class, () -> authorService.invalidId(0));
+        Assertions.assertThrows(InvalidIdException.class, () -> authorService.invalidId(-1));
+    }
+
+    @Test
+    @DisplayName("It shouldn't throw any NumberFormatException when the id is valid")
+    void deleteById_DoesNotThrowAnyNumberFormatException_WhenValidId(){
+        String scannerTest = "1\ny";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+       Assertions.assertDoesNotThrow(() -> authorService.delete());
+    }
+
+    @Test
+    @DisplayName("If the id is null, a NumberFormatException should be thrown")
+    void update_ThrowNumberFormatException_WhenFieldIsEmpty(){
+        String scannerTest = "\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertThrows(NumberFormatException.class, () -> authorService.update());
+    }
+    @Test
+    @DisplayName("If the author was not found, an IllegalArgumentException should be thrown")
+    void update_ThrowIllegalArgumentException_WhenAuthorWasNotFound(){
+        String scannerTest = "1000";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> authorService.update());
+    }
+    @Test
+    @DisplayName("If the id is null a NumberFormatException should be thrown")
+    void findAuthorByWroteBook_ThrowNumberFormatException_WhenFieldIsEmpty(){
+        String scannerTest = "\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertThrows(NumberFormatException.class, () -> authorService.findAuthorByWroteBook());
+    }
+
+    @Test
+    @DisplayName("It shouldn't throw any NumberFormatException when the id is valid")
+    void findAuthorByWroteBook_DoesNotThrowAnyNumberFormatException_WhenValidId(){
+        String scannerTest = "1";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(scannerTest.getBytes());
+        Scanner sc = new Scanner(inputStream);
+        authorService = new AuthorService(sc, authorDao);
+        Assertions.assertDoesNotThrow(() -> authorService.findAuthorByWroteBook());
+    }
+}
