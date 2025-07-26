fe61f45c040a8d768447609841601cfc5d12621b
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-26 14:03:24 -0300

    fix: injecting a DaoFactory dependencies when instantiate the services classes to create the Daos impl, and receiving the scanner by constructor. Surrounding all menus with a try catch bÃlock to deal with a unchecked exceptions

--- src/main/java/br/com/sabrinaweb/appbiblioteca/view/LibraryProgram.java
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/view/LibraryProgram.java
@@ -1,16 +1,21 @@
 package br.com.sabrinaweb.appbiblioteca.view;
 
+import br.com.sabrinaweb.appbiblioteca.model.dao.DaoFactory;
 import br.com.sabrinaweb.appbiblioteca.model.service.AuthorService;
 import br.com.sabrinaweb.appbiblioteca.model.service.BookService;
 import br.com.sabrinaweb.appbiblioteca.model.service.LibraryLoanService;
 import br.com.sabrinaweb.appbiblioteca.model.service.UserService;
 import lombok.extern.log4j.Log4j2;
 
+import java.util.Scanner;
+
 @Log4j2
 public class LibraryProgram {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("+.°.+.°. Welcome to our library! +.+°.°+.");
-        MenuMain menu = new MenuMain(new BookService(), new AuthorService(), new LibraryLoanService(), new UserService());
+        MenuMain menu = new MenuMain(new BookService(SCANNER, DaoFactory.createBookDao()), new AuthorService(SCANNER, DaoFactory.createAuthorDao()), new LibraryLoanService(SCANNER, DaoFactory.createLoanDao()), new UserService(SCANNER, DaoFactory.createUserDao()), SCANNER);
         menu.menu();
     }
 }
--- src/main/java/br/com/sabrinaweb/appbiblioteca/view/MenuMain.java
+++ src/main/java/br/com/sabrinaweb/appbiblioteca/view/MenuMain.java
@@ -1,5 +1,8 @@
 package br.com.sabrinaweb.appbiblioteca.view;
 
+import br.com.sabrinaweb.appbiblioteca.model.exceptions.BookNotAvailableForLoanException;
+import br.com.sabrinaweb.appbiblioteca.model.exceptions.InvalidLoanException;
+import br.com.sabrinaweb.appbiblioteca.model.exceptions.UserNotFoundException;
 import br.com.sabrinaweb.appbiblioteca.model.service.AuthorService;
 import br.com.sabrinaweb.appbiblioteca.model.service.BookService;
 import br.com.sabrinaweb.appbiblioteca.model.service.LibraryLoanService;
@@ -15,40 +18,44 @@ public class MenuMain {
     private LibraryLoanService libraryLoanService;
     private UserService userService;
 
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private Scanner scanner;
 
-    public MenuMain(BookService bookService, AuthorService authorService, LibraryLoanService libraryLoanService, UserService userService) {
+    public MenuMain(BookService bookService, AuthorService authorService, LibraryLoanService libraryLoanService, UserService userService, Scanner scanner) {
         this.bookService = bookService;
         this.authorService = authorService;
         this.libraryLoanService = libraryLoanService;
         this.userService = userService;
+        this.scanner = scanner;
     }
+
     public MenuMain() {
     }
-    public void menu(){
+
+    public void menu() {
         try {
             showMenuOptions();
-            int menuResp = Integer.parseInt(SCANNER.nextLine());
+            int menuResp = Integer.parseInt(scanner.nextLine());
             do {
-                switch (menuResp){
+                switch (menuResp) {
                     case 1 -> userMenu();
                     case 2 -> bookMenu();
                     case 3 -> loanMenu();
                     case 4 -> authorMenu();
                     case 5 -> reportMenu();
                     case 0 -> System.out.println("Exiting...");
-                    default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+                    default -> throw new IllegalArgumentException("Invalid option you must enter the available options. Exiting...");
                 }
-                if (menuResp != 0){
+                if (menuResp != 0) {
                     showMenuOptions();
-                    menuResp = Integer.parseInt(SCANNER.nextLine());
+                    menuResp = Integer.parseInt(scanner.nextLine());
                 }
-            }while (menuResp != 0);
-        }catch (IllegalArgumentException e){
+            } while (menuResp != 0);
+        } catch (IllegalArgumentException e) {
             log.error(e);
         }
     }
-    public void showMenuOptions(){
+
+    public void showMenuOptions() {
         System.out.println("--+--Choose a operation--+--");
         System.out.println("1 - User");
         System.out.println("2 - Book");
@@ -58,105 +65,131 @@ public class MenuMain {
         System.out.println("0 - >> EXIT");
         System.out.print("RESP: ");
     }
-    public void bookMenu(){
-        System.out.println("1 - Insert new book");
-        System.out.println("2 - Update a book");
-        System.out.println("3 - Delete a book");
-        System.out.println("4 - Find book by title");
-        System.out.println("5 - Find available books");
-        System.out.println("6 - Find book(s) of a author");
-        System.out.println("9 - >> Return to the main menu");
 
-        System.out.print("RESP: ");
-        int resp = Integer.parseInt(SCANNER.nextLine());
+    public void bookMenu() {
+        try {
+            System.out.println("1 - Insert new book");
+            System.out.println("2 - Update a book");
+            System.out.println("3 - Delete a book");
+            System.out.println("4 - Find book by title");
+            System.out.println("5 - Find available books");
+            System.out.println("6 - Find book(s) of an author");
+            System.out.println("9 - >> Return to the main menu");
 
-        switch (resp){
-            case 1 -> bookService.insert();
-            case 2 -> bookService.update();
-            case 3 -> bookService.delete();
-            case 4 -> bookService.findByTitle();
-            case 5 -> bookService.findAvailableBooks();
-            case 6 -> bookService.findBookOfAAuthor();
-            case 9 -> System.out.println("Coming back to the principal menu...");
-            default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
-        }
-    }
-    public void userMenu(){
-        System.out.println("1 - Insert new user");
-        System.out.println("2 - Update a user");
-        System.out.println("3 - Delete a user");
-        System.out.println("4 - Find user by name");
-        System.out.println("9 - >> Return to the main menu");
+            System.out.print("RESP: ");
+            int resp = Integer.parseInt(scanner.nextLine());
 
-        System.out.print("RESP: ");
-        int resp = Integer.parseInt(SCANNER.nextLine());
-        switch (resp){
-            case 1 -> userService.insert();
-            case 2 -> userService.update();
-            case 3 -> userService.delete();
-            case 4 -> userService.findByName();
-            case 9 -> System.out.println("Coming back to the principal menu...");
-            default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
-        }
-    }
-    public void authorMenu(){
-        System.out.println("1 - Insert new author");
-        System.out.println("2 - Update a author");
-        System.out.println("3 - Delete a author");
-        System.out.println("4 - Find author by name");
-        System.out.println("5 - Find author by their book");
-        System.out.println("9 - >> Return to the main menu");
-
-        System.out.print("RESP: ");
-        int resp = Integer.parseInt(SCANNER.nextLine());
-        switch (resp){
-            case 1 -> authorService.insert();
-            case 2 -> authorService.update();
-            case 3 -> authorService.delete();
-            case 4 -> authorService.findByName();
-            case 5 -> authorService.findAuthorByWroteBook();
-            case 9 -> System.out.println("Coming back to the principal menu...");
-            default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            switch (resp) {
+                case 1 -> bookService.insert();
+                case 2 -> bookService.update();
+                case 3 -> bookService.delete();
+                case 4 -> bookService.findByTitle();
+                case 5 -> bookService.findAvailableBooks();
+                case 6 -> bookService.findBookOfAAuthor();
+                case 9 -> System.out.println("Coming back to the principal menu...");
+                default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            }
+        } catch (IllegalArgumentException e) {
+            log.error(e);
         }
     }
 
-    public void loanMenu(){
-        System.out.println("1 - Register a new loan");
-        System.out.println("2 - Return a book");
-        System.out.println("3 - Find the book in the loan service by its status or by the loan status");
-        System.out.println("4 - Find the user(s) with more than one loan");
-        System.out.println("5 - Find all loans");
-        System.out.println("9 - >> Return to the main menu");
+    public void userMenu() {
+        try {
+            System.out.println("1 - Insert new user");
+            System.out.println("2 - Update a user");
+            System.out.println("3 - Delete a user");
+            System.out.println("4 - Find user by name");
+            System.out.println("9 - >> Return to the main menu");
 
-        System.out.print("RESP: ");
-        int resp = Integer.parseInt(SCANNER.nextLine());
-        switch (resp){
-            case 1 -> libraryLoanService.registerLoan();
-            case 2 -> libraryLoanService.returnBook();
-            case 3 -> libraryLoanService.findBooksBorrowedByStatus();
-            case 4 -> libraryLoanService.findUsersWithMoreThanOneBookBorrowed();
-            case 5 -> libraryLoanService.findAllLoan();
-            case 9 -> System.out.println("Coming back to the principal menu...");
-            default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            System.out.print("RESP: ");
+            int resp = Integer.parseInt(scanner.nextLine());
+            switch (resp) {
+                case 1 -> userService.insert();
+                case 2 -> userService.update();
+                case 3 -> userService.delete();
+                case 4 -> userService.findByName();
+                case 9 -> System.out.println("Coming back to the principal menu...");
+                default ->
+                        throw new IllegalArgumentException("Invalid option you must enter the available options :).");
+            }
+        } catch (IllegalArgumentException e) {
+            log.error(e);
         }
     }
-    public void reportMenu(){
-        System.out.println("1 - Book more borrowed");
-        System.out.println("2 - Users with more loan");
-        System.out.println("3 - Books borrowed in the moment");
-        System.out.println("4 - Generate a payment fee for late loans");
-        System.out.println("9 - >> Return to the main menu");
 
-        System.out.print("RESP: ");
-        int resp = Integer.parseInt(SCANNER.nextLine());
+    public void authorMenu() {
+        try {
+            System.out.println("1 - Insert new author");
+            System.out.println("2 - Update a author");
+            System.out.println("3 - Delete a author");
+            System.out.println("4 - Find author by name");
+            System.out.println("5 - Find author by their book");
+            System.out.println("9 - >> Return to the main menu");
 
-        switch (resp){
-            case 1 -> bookService.bookMoreBorrowed();
-            case 2 -> userService.findUserWithTheMostLoans();
-            case 3 -> libraryLoanService.bookBorrowedInTheMoment();
-            case 4 -> libraryLoanService.penaltyFeeForLateLoans();
-            case 9 -> System.out.println("Coming back to the principal menu...");
-            default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            System.out.print("RESP: ");
+            int resp = Integer.parseInt(scanner.nextLine());
+            switch (resp) {
+                case 1 -> authorService.insert();
+                case 2 -> authorService.update();
+                case 3 -> authorService.delete();
+                case 4 -> authorService.findByName();
+                case 5 -> authorService.findAuthorByWroteBook();
+                case 9 -> System.out.println("Coming back to the principal menu...");
+                default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            }
+        } catch (IllegalArgumentException e) {
+            log.error(e);
+        }
+    }
+
+    public void loanMenu() {
+        try {
+            System.out.println("1 - Register a new loan");
+            System.out.println("2 - Return a book");
+            System.out.println("3 - Find the book in the loan service by its status or by the loan status");
+            System.out.println("4 - Find the user(s) with more than one loan");
+            System.out.println("5 - Find all loans");
+            System.out.println("9 - >> Return to the main menu");
+
+            System.out.print("RESP: ");
+            int resp = Integer.parseInt(scanner.nextLine());
+            switch (resp) {
+                case 1 -> libraryLoanService.registerLoan();
+                case 2 -> libraryLoanService.returnBook();
+                case 3 -> libraryLoanService.findBooksBorrowedByStatus();
+                case 4 -> libraryLoanService.findUsersWithMoreThanOneBookBorrowed();
+                case 5 -> libraryLoanService.findAllLoan();
+                case 9 -> System.out.println("Coming back to the principal menu...");
+                default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            }
+        } catch (IllegalArgumentException | InvalidLoanException | IllegalStateException |
+                 BookNotAvailableForLoanException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public void reportMenu() {
+        try {
+            System.out.println("1 - Book more borrowed");
+            System.out.println("2 - Users with more loan");
+            System.out.println("3 - Books borrowed in the moment");
+            System.out.println("4 - Generate a payment fee for late loans");
+            System.out.println("9 - >> Return to the main menu");
+
+            System.out.print("RESP: ");
+            int resp = Integer.parseInt(scanner.nextLine());
+
+            switch (resp) {
+                case 1 -> bookService.theMostBorrowedBook();
+                case 2 -> userService.findUserWithTheMostLoans();
+                case 3 -> libraryLoanService.bookBorrowedInTheMoment();
+                case 4 -> libraryLoanService.penaltyFeeForLateLoans();
+                case 9 -> System.out.println("Coming back to the principal menu...");
+                default -> throw new IllegalArgumentException("Invalid option you must enter the available options :)");
+            }
+        }catch (IllegalArgumentException e){
+            log.error(e.getMessage());
         }
     }
 }
